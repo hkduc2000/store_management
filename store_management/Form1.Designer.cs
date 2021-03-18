e4dd15670d4e856d5ddb8432030e5bd241839171
Hoang Kim Duc <duchk1@fsoft.com.vn>
2021-03-18 16:01:06 +0700

    '.'

--- store_management/Form1.Designer.cs
+++ store_management/Form1.Designer.cs
@@ -23,13 +23,46 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            this.components = new System.ComponentModel.Container();
+            this.trvMenu = new System.Windows.Forms.TreeView();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // trvMenu
+            // 
+            this.trvMenu.Location = new System.Drawing.Point(12, 12);
+            this.trvMenu.Name = "trvMenu";
+            this.trvMenu.Size = new System.Drawing.Size(224, 597);
+            this.trvMenu.TabIndex = 0;
+            this.trvMenu.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.trvMenu_AfterSelect);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(253, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(567, 597);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(832, 621);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.trvMenu);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TreeView trvMenu;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
 
--- store_management/Program.cs
+++ store_management/Program.cs
@@ -5,6 +5,52 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace store_management {
+
+    class CategoryTreeNode : TreeNode {
+        int categoryID;
+
+        public CategoryTreeNode(string text, int categoryID) : base(text) {
+            this.categoryID = categoryID;
+        }
+
+        public CategoryTreeNode(string text, CategoryTreeNode[] children, int categoryID) : base(text,children) {
+            this.categoryID = categoryID;
+        }
+
+        public int CategoryID { get => categoryID; set => categoryID = value; }
+    }
+
+    class Product {
+        int productID;
+        string productName;
+        string description;
+        int costPrice;
+        int price;
+        int quantity;
+        int quantityID;
+
+        public Product() {
+        }
+
+        public Product(int productID, string productName, string description, int costPrice, int price, int quantity, int quantityID) {
+            this.ProductID = productID;
+            this.ProductName = productName;
+            this.Description = description;
+            this.CostPrice = costPrice;
+            this.Price = price;
+            this.Quantity = quantity;
+            this.QuantityID = quantityID;
+        }
+
+        public int ProductID { get => productID; set => productID = value; }
+        public string ProductName { get => productName; set => productName = value; }
+        public string Description { get => description; set => description = value; }
+        public int CostPrice { get => costPrice; set => costPrice = value; }
+        public int Price { get => price; set => price = value; }
+        public int Quantity { get => quantity; set => quantity = value; }
+        public int QuantityID { get => quantityID; set => quantityID = value; }
+    }
+
     static class Program {
         /// <summary>
         /// The main entry point for the application.
